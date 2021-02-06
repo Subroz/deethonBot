287b9486ff4598cbb15f81dce2052ad216042be4
TomyPrs <tomy.prs25@gmail.com>
2021-02-06 08:23:59 +0700

    try disable uvloop
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>

--- deegram/__init__.py
+++ deegram/__init__.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 import deethon
-import uvloop
+#import uvloop
 from dotenv import load_dotenv
 from telethon import TelegramClient, functions, types
 from telethon.events import NewMessage
@@ -56,7 +56,7 @@ logger.info("Bot started")
 # Saving user preferences locally
 users = {}
 
-uvloop.install()
+#uvloop.install()
 bot.loop.run_until_complete(
     bot(
         functions.bots.SetBotCommandsRequest(
